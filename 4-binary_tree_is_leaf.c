f0b916f1741a8c0b58ea185321399785a72a050f
Daniel Igwe <daniel.igwe000@gmail.com>
2022-11-21 12:19:01 +0100

    Is leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -10,7 +10,10 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if ((node->left == NULL) && (node->right == NULL))
-		return (1);
+	if (node != NULL)
+	{
+		if ((node->left == NULL) && (node->right == NULL))
+			return (1);
+	}
 	return (0);
 }
