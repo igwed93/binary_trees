c8d62140d8d6eb7f6f5b3ddf940fc6f849df66a5
Daniel Igwe <daniel.igwe000@gmail.com>
2022-11-22 23:20:45 +0100

    Nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -12,6 +12,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (!tree || (!tree->left && !tree->right))
 		return (0);
-	
+
 	return (binary_tree_nodes(tree->left) + 1 + binary_tree_nodes(tree->right));
 }
